1a38c30b8d77f48d2220c87e21e6352828134a4c
Radha Raman Jha <radharamanjha1989@gmail.com>
2023-04-29 15:35:17 +0530

    data insertion in SQL table

---
+++ data_insertion_customer_table.sql
@@ -0,0 +1,10 @@
+/* INSERTING DATA IN Customer Table */
+INSERT INTO customer(customer_id, FirstName, LastName, BirthDay, PhoneNumber, EmailID)
+VALUES(1,"Achishhek", "Singh", '1987-11-21', 877676768, "rafdn@gmail.com"),
+      (2,"Bina", "Rachel", '1987-10-21', 877676767,"afdn@gmail.com"),
+	(3,"Camela","squars", '1987-10-22',91194227, "adn@gmail.com"),
+	(4, "Dino", "Jha",'1987-10-22',989119422,"adn@gmail.com"),
+      (5, "Emily", "Ground",'1989-12-22',91194227,"bdn@gmail.com"),
+      (6, "Fergie", "Rachel",'1919-12-22',91194227,"cdn@gmail.com"),
+	(7, "Eddi", "Guerrero",'1980-10-22',91194227,"edn@gmail.com"),
+	(8, "Jack", "Mysterious",'1980-10-22',null, "jdn@gmail.com");
---
+++ update_data_customer_table.sql
@@ -0,0 +1,4 @@
+/*Updating the PhoneNumber Data in Customer Table*/
+UPDATE customer 
+SET  PhoneNumber = 91194225
+Where customer_id = 8;
