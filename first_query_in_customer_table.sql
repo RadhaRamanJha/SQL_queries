b4919f45a06a9aab0aeb126478e4f2d1b84cd267
Radha Raman Jha <radharamanjha1989@gmail.com>
2023-05-01 15:03:20 +0530

    MySQL query and groupby

---
+++ first_query_in_customer_table.sql
@@ -0,0 +1,2 @@
+SELECT customer_id, FirstName, LastName, PhoneNumber
+FROM customer;
---
+++ insert_data_customer_address_table.sql
@@ -0,0 +1,17 @@
+/*INSERTING DATA IN ADDRESS Table */
+INSERT INTO customer_address(customer_id, full_address )
+	VALUES(1,"India, Arunachal Pradesh , Mizoam, Magun puram society, siddhi tower Flat no 1218"),
+		  (2,"India, Madhya Pradesh , Bombay, guru ghantal society, riddhi Flat no 12233"),
+		  (3,"India, West Benagal , Calculta, Bhattacharya society, riddhi Flat no 12233"),
+		  (3,"America, South Amptan , California, Indian society, peterson tower, Flat no 12233"),
+		  (4,"America, New jersy , Albania, Indian society, tom lathman tower, Flat no 12233"),
+		  (5,"South Africa, west verginia , California, Indian society, timba babuma tower, Flat no 12233"),
+		  (6,"West inies, Carraibian  , coconut beach, Indian society, Brain lara tower, Flat no 12233"),
+		  (7,"West indies, new port  , coconut beach, Indian society, Ambrose tower, Flat no 12233"),
+		  (8,"West indies, new port  , coconut beach, Indian society, Ambrose tower, Flat no 12233"),
+		  (8,"West indies, Carrabian Islands  , sunset beach, Indian society, Ambrose tower, Flat no 12233"),
+		  (1,"nglland,  rrabian Islands  , sunrise beach, Indian society, peterson tower, Flat no 12233"),
+		  (2,"nglland, Carrabian Islands  , midsun beach, Indian society, Ambrose tower, Flat no 12233"),
+		  (2,"ustralia, Syndney  , ponting road, Indian society, hayden tower, Flat no 12233"),
+		  (4,"ustralia, Syndney  , hayden road, Indian society, hayden tower, Flat no 12233"),
+		  (5,"ustralia, Syndney  , ponting road, Indian society, hayden tower, Flat no 12233");
