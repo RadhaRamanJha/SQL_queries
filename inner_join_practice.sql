cdab3f6ebdd9b61e07c24b048be67e270726cb40
Radha Raman Jha <radharamanjha1989@gmail.com>
2023-06-02 19:56:41 +0530

    inner join

---
+++ inner_join_practice.sql
@@ -0,0 +1,12 @@
+-- 
+use sakila;
+SELECT 
+    f.title, c.name
+FROM
+    film f
+        INNER JOIN
+    film_category fc ON f.film_id = fc.film_id
+        INNER JOIN
+    category c ON fc.category_id = c.category_id
+WHERE
+    c.name = 'Action'
