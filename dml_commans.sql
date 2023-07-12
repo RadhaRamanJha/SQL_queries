c12a3a225da3aa8d52f58af0534c4a14106221c2
Radha Raman Jha <radharamanjha1989@gmail.com>
2023-07-12 07:51:27 +0530

    dml_commands

---
+++ dml_commans.sql
@@ -0,0 +1,17 @@
+use exelr;
+desc people;
+
+## DML notes #####
+# While inserting the da
+
+# insert
+Insert into people(id,name,age,gender,phonenumber,adharnumber)
+values(1,'radha',35,'Male',97212,2812871),
+(2,'rasdsdha',30,'Female',661822,2822871),
+(3,'jdisddjsk',34,'Male',462129,781291);
+
+###### truncate table
+truncate table people;
+
+############ decribe table ##############
+desc people;
