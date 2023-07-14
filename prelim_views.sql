d0d557f7195409af24bcb33e6470565c21e9fd1a
Radha Raman Jha <radharamanjha1989@gmail.com>
2023-07-14 22:25:03 +0530

    first_view_in_sql

---
+++ prelim_views.sql
@@ -0,0 +1,72 @@
+use sakila;
+-- creating view
+CREATE VIEW cus_view (customer_id , first_name , last_name , email_id) AS
+    SELECT 
+        customer_id,
+        first_name,
+        last_name,
+        CONCAT(SUBSTRING(email, 1, 2),
+                '****',
+                SUBSTRING(email, - 4)) email_id
+    FROM
+        customer;
+-- sleecting first 10 rwos and column from view
+SELECT 
+    *
+FROM
+    cus_view
+LIMIT 10;
+-- select 2 rows from table_view
+SELECT 
+    first_name, email_id
+FROM
+    cus_view
+LIMIT 10;
+## group query agianst cus_view 
+-- 1 
+SELECT 
+    first_name, COUNT(*), MAX(first_name), MIN(last_name)
+FROM
+    cus_view
+GROUP BY 1
+HAVING COUNT(*) > 1
+ORDER BY 1;
+
+-- 2 
+SELECT 
+    first_name, COUNT(*), MAX(first_name), MAX(last_name)
+FROM
+    cus_view
+GROUP BY 1
+HAVING COUNT(*) > 1
+ORDER BY 1;
+-- using inner join on view and payment table
+SELECT 
+    cv.first_name, cv.last_name, p.amount
+FROM
+    cus_view cv
+        INNER JOIN
+    payment p ON cv.customer_id = p.customer_id
+WHERE
+    p.amount >= 11;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
