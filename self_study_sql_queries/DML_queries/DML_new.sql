026da4ab615edb985ed143c0ea3b3e3edf2e4a09
Radha Raman Jha <radharamanjha1989@gmail.com>
2023-10-09 19:46:46 +0530

    DML query revision

---
+++ self_study_sql_queries/DML_queries/DML_new.sql
@@ -0,0 +1,19 @@
+#DML
+
+#insert query
+insert into p_info(id,emp_name,age)
+values(1,"sam",25),
+(2,'john',30),
+(3,'july',28),
+(4,'reena',21);
+
+update p_info set emp_name='ram'
+where Id=1;
+
+-- truncate table p_info;
+
+delete from p_info;
+
+
+
+
