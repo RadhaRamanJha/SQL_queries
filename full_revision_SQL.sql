4ea9f6b3c3731a496c2f50be4d6efb45aee53e78
Radha Raman Jha <radharamanjha1989@gmail.com>
2024-05-31 20:16:40 +0530

    SQL Revision update

---
+++ full_revision_SQL.sql
@@ -0,0 +1,14 @@
+## See all available Databases
+show databases;
+## Select current data and time 
+select now();
+### Select current data
+select current_date();
+### Select current time stamp
+select current_timestamp();
+##  select 
+select current_time();
+## select current user
+select current_user();
+## supported chaaracter sets in MySQL -- Default is utf8mb4
+show character set
